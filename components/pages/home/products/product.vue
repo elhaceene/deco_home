af89b568ce1694b805862221298b06b1bf94c85b
elhacene <belhaouarihacen@gmail.com>
2024-10-23 18:39:20 +0100

    :+1:  Update credentials data

--- components/pages/home/products/product.vue
+++ components/pages/home/products/product.vue
@@ -1,72 +1,65 @@
 <script setup lang="ts">
-const {t} = useI18n();
+const { t } = useI18n();
 const localePath = useLocalePath();
 
 interface product {
-  id : number,
-  category : string;
-  name : string;
-  price : number,
-  img : string;
-  sale? : number;
-
+  id: number;
+  category: string;
+  name: string;
+  price: number;
+  img: string;
+  sale?: number;
 }
 
 const props = defineProps({
   product: {
     type: Object as () => product,
-    required: true, 
+    required: true,
   },
-
 });
 
-const category = computed(()=>{
-  if (props.product.category === "chair"){
-  return t('common.chair');
-}else if (props.product.category === "couch"){
-  return t('common.couch');
-}else if (props.product.category === "lamp"){
-  return t('common.lamp');
-}else if (props.product.category === "table"){
-  return t('common.table');
-}else {
-  return t('common.bed');
-}
+const category = computed(() => {
+  if (props.product.category === "chair") {
+    return t("common.chair");
+  } else if (props.product.category === "couch") {
+    return t("common.couch");
+  } else if (props.product.category === "lamp") {
+    return t("common.lamp");
+  } else if (props.product.category === "table") {
+    return t("common.table");
+  } else {
+    return t("common.bed");
+  }
 });
 
-const sale = computed(()=>{
-  if (!props.product.sale){
+const sale = computed(() => {
+  if (!props.product.sale) {
     return 0;
   }
 
- return props.product.sale * 100;
+  return props.product.sale * 100;
 });
 
-
-const price = computed(()=>{
-
-  if (!props.product.sale){
+const price = computed(() => {
+  if (!props.product.sale) {
     return props.product.price;
   }
   const amontToSale = props.product.price * props.product.sale;
 
   return props.product.price - amontToSale;
 });
-
-
 </script>
 
 <template>
-  <nuxt-link :to="localePath('/products/'+ product.id)" class="product">
+  <nuxt-link :to="localePath('/products/' + product.id)" class="product">
     <span class="img-container">
-      <img :src="product.img" :alt="product.name">
+      <img :src="product.img" :alt="product.name" />
       <span v-if="product.sale" class="tag">
-        {{ sale }}% {{ $t('common.reduction') }}
+        {{ sale }}% {{ $t("common.reduction") }}
       </span>
     </span>
 
     <span class="body">
-
       <span class="title-container">
         <span class="title">
           {{ product.name }}
@@ -77,102 +70,91 @@ const price = computed(()=>{
       </span>
 
       <span class="price-container">
-        <span class="price">
-          {{ price }} DZD
-        </span>
+        <span class="price"> {{ price }} DZD </span>
         <span v-if="product.sale" class="old-price">
           {{ product.price }} DZD
         </span>
       </span>
-
     </span>
   </nuxt-link>
 </template>
 
 <style lang="scss" scoped>
-
-.product{
+.product {
   width: calc((100% / 3) - 16px);
   display: flex;
   flex-direction: column;
   gap: 16px;
 
-
-  @media (max-width:$tablet){
+  @media (max-width: $tablet) {
     width: calc((100% / 2) - 16px);
   }
 
-  @media (max-width:$phone){
+  @media (max-width: $phone) {
     width: 100%;
   }
 
-  .img-container{
+  .img-container {
     width: 100%;
-    height: 400px;
+    height: 350px;
     border-radius: 20px 20px 0 0;
     position: relative;
     overflow: hidden;
 
-    img{
+    img {
       width: 100%;
       border-radius: 20px 20px 0 0;
       object-fit: cover;
       object-position: center;
     }
 
-    .tag{
+    .tag {
       position: absolute;
       top: 24px;
       left: 24px;
-      
     }
-
   }
 
-  .body{
+  .body {
     display: flex;
     align-items: flex-start;
     justify-content: space-between;
     padding: 0 16px;
+    margin-bottom: 60px;
 
-    .title-container, .price-container{
+    .title-container,
+    .price-container {
       display: flex;
       flex-direction: column;
       gap: 6px;
-    
 
-    .title, .price{
-      @include text-lg;
+      .title,
+      .price {
+        @include text-lg;
         font-weight: 500;
         color: $black;
-    }
+      }
 
-    .category, .old-price{
+      .category,
+      .old-price {
         @include text-sm;
         font-weight: 500;
         color: $neutral100;
       }
 
-    .old-price{
+      .old-price {
         text-decoration: line-through;
       }
     }
   }
 
-  &:hover{
-    .img-container img{
+  &:hover {
+    .img-container img {
       transform: scale(1.1);
     }
-    .body .title{
+    .body .title {
       color: $accent300;
     }
   }
 }
-
-  
-
-
-
-
-
 </style>
