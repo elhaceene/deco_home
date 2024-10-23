82afb9e7ef5a0e3a96a0bcbdcc9d52a0337ae097
elhacene <belhaouarihacen@gmail.com>
2024-10-23 18:12:19 +0100

    :100:  Update credentials data

--- components/layout/MobileMenu.vue
+++ components/layout/MobileMenu.vue
@@ -1,54 +1,70 @@
 <script setup lang="ts">
-  const localePath = useLocalePath();
+const localePath = useLocalePath();
 </script>
 
 <template>
   <div class="mobile-menu">
-   <div class="header">
-    <nuxt-link :to="localePath('/')" :title="$t('common.home')" >
-    <img src="/public/logo.svg" alt="Logo Decohome" width="172" height="32">
-    </nuxt-link>
-    <button type="button" class="btn-square md" @click="$emit('close-mobile-menu')">
-      <Icon name="heroicons:x-mark" />
-    </button>
-   </div>
+    <div class="header">
+      <nuxt-link :to="localePath('/')" :title="$t('common.home')">
+        <img
+          src="/public/logo.svg"
+          alt="Logo Decohome"
+          width="172"
+          height="32"
+        />
+      </nuxt-link>
+      <button
+        type="button"
+        class="btn-square md"
+        @click="$emit('close-mobile-menu')"
+      >
+        <Icon name="heroicons:x-mark" />
+      </button>
+    </div>
 
-   <div class="links">
-    <nuxt-link :to="localePath('/')" :title="$t('common.home')" 
-    class="link base lg" @click.enter="$emit('close-mobile-menu')" >{{$t("common.home")}}</nuxt-link>
+    <div class="links">
+      <nuxt-link
+        :to="localePath('/')"
+        :title="$t('common.home')"
+        class="link base lg"
+        @click.enter="$emit('close-mobile-menu')"
+        >{{ $t("common.home") }}</nuxt-link
+      >
 
-    <nuxt-link :to="localePath('/faq')" :title="$t('common.faq')" 
-    class="link base lg" @click.enter="$emit('close-mobile-menu')" >{{$t("common.faq")}}</nuxt-link>
-  </div>
-
+      <nuxt-link
+        :to="localePath('/faq')"
+        :title="$t('common.faq')"
+        class="link base lg"
+        @click.enter="$emit('close-mobile-menu')"
+        >{{ $t("common.faq") }}</nuxt-link
+      >
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.mobile-menu{
+.mobile-menu {
   width: 100%;
   height: 100vh;
   background-color: white;
-
   position: fixed;
   top: 0;
   left: 0;
   z-index: 999;
   padding: 16px;
   display: flex;
-    flex-direction: column;
-    gap: 60px;
-  .header{
+  flex-direction: column;
+  gap: 60px;
+
+  .header {
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
-  .links{
+  .links {
     display: flex;
     flex-direction: column;
     gap: 32px;
   }
 }
-
-
 </style>
--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,61 +1,83 @@
 <script setup lang="ts">
-  const localePath = useLocalePath();
-  const showLanguages = ref(false);
-  const {locale} = useI18n();
-  const flag = computed(() => {
-    if (locale.value==='en'){
-      return "flag:us-4x3";
-    } else if (locale.value==='ar'){
-      return "flag:dz-4x3";
-    }
+const localePath = useLocalePath();
+const showLanguages = ref(false);
+const { locale } = useI18n();
+const flag = computed(() => {
+  if (locale.value === "en") {
+    return "flag:us-4x3";
+  } else if (locale.value === "ar") {
+    return "flag:dz-4x3";
+  }
 
-    return "flag:fr-4x3";
-  });
+  return "flag:fr-4x3";
+});
 
+const cartStore = useCartStore();
 
-  const cartStore = useCartStore();
-
-  const cartQuantity = computed(()=>{
-    return cartStore.cart.reduce((total, Product)=> total + Product.quantity, 0)
-  })
+const cartQuantity = computed(() => {
+  return cartStore.cart.reduce((total, Product) => total + Product.quantity, 0);
+});
 </script>
 
 <template>
   <nav class="navbar">
-  <nuxt-link :to="localePath('/')" :title="$t('common.home')" >
-    <img src="/public/logo.svg" alt="Logo Decohome" width="172" height="32">
-  </nuxt-link>
+    <nuxt-link :to="localePath('/')" :title="$t('common.home')">
+      <img src="/public/logo.svg" alt="Logo Decohome" width="172" height="32" />
+    </nuxt-link>
 
-  <div class="links">
-    <nuxt-link :to="localePath('/')" :title="$t('common.home')" class="link base">{{$t("common.home")}}</nuxt-link>
-    <nuxt-link :to="localePath('/faq')" :title="$t('common.faq')" class="link base">{{$t("common.faq")}}</nuxt-link>
-  </div>
-
-  <div class="actions">
-    <div class="languages-wraper">
-      <button type="button" :title="$t('common.lang')" class="btn-square md" @click="showLanguages=!showLanguages">
-      <icon :name="flag"  />
-     </button>
-
-     <LayoutLanguagesDropdown v-if="showLanguages"/>
+    <div class="links">
+      <nuxt-link
+        :to="localePath('/')"
+        :title="$t('common.home')"
+        class="link base"
+        >{{ $t("common.home") }}</nuxt-link
+      >
+      <nuxt-link
+        :to="localePath('/faq')"
+        :title="$t('common.faq')"
+        class="link base"
+        >{{ $t("common.faq") }}</nuxt-link
+      >
     </div>
-    
-    <NuxtLink :to="localePath('/cart')" :title="$t('common.cart')" class="btn-square md" >
-    <Icon name="heroicons:shopping-bag" />
-    <span class="counter">
-      {{ cartQuantity }}
-    </span>
-  </NuxtLink>
-  <button type="button" :title="$t('common.menu')" 
-  class="btn-square md burger-menu" @click="$emit('open-mobile-menu')"> 
-    <icon name="heroicons:bars-3"/>
-  </button>
-  </div>
+
+    <div class="actions">
+      <div class="languages-wraper">
+        <button
+          type="button"
+          :title="$t('common.lang')"
+          class="btn-square md"
+          @click="showLanguages = !showLanguages"
+        >
+          <icon :name="flag" />
+        </button>
+
+        <LayoutLanguagesDropdown v-if="showLanguages" />
+      </div>
+
+      <NuxtLink
+        :to="localePath('/cart')"
+        :title="$t('common.cart')"
+        class="btn-square md"
+      >
+        <Icon name="heroicons:shopping-bag" />
+        <span class="counter">
+          {{ cartQuantity }}
+        </span>
+      </NuxtLink>
+      <button
+        type="button"
+        :title="$t('common.menu')"
+        class="btn-square md burger-menu"
+        @click="$emit('open-mobile-menu')"
+      >
+        <icon name="heroicons:bars-3" />
+      </button>
+    </div>
   </nav>
 </template>
 
 <style lang="scss" scoped>
-.navbar{
+.navbar {
   width: 100%;
   height: 80px;
   background-color: white;
@@ -67,33 +89,32 @@
   top: 0;
   left: 0;
   z-index: 998;
-  @media (max-width:$phone){
+  @media (max-width: $phone) {
     padding: 0 16px;
   }
 
-
-  .links, .actions{
+  .links,
+  .actions {
     display: flex;
     align-items: center;
   }
   .links {
     justify-content: center;
     gap: 32px;
-    @media (max-width : $phone){
-            display: none;
-        }
-
+    @media (max-width: $phone) {
+      display: none;
+    }
   }
-  .actions{
+  .actions {
     justify-content: flex-end;
     gap: 8px;
   }
-  .burger-menu{
-        @media (min-width : $phone){
-            display: none;
-        }
+  .burger-menu {
+    @media (min-width: $phone) {
+      display: none;
     }
-  .languages-wraper{
+  }
+  .languages-wraper {
     position: relative;
   }
 }
