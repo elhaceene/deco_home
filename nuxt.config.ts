4e0cc72d831ee45cf4fdc0df9c37927539a120b2
elhacene <belhaouarihacen@gmail.com>
2024-10-23 15:07:18 +0100

    :+1:  Update credentials data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,8 +10,8 @@ export default defineNuxtConfig({
       },
 
       meta: [
-        { charset: "UTF-8" },
-        { name: "viewport", content: "width=device-width, initialscale=1" },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
         {
           hid: "description",
           name: "description",
